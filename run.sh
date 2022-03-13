c1af2122ee445a04815758be089695b36c408b74
aarati_K <aarati_K@node.aarati-k-120560.quickstep-pg0.wisc.cloudlab.us>
2022-03-13 13:54:06 -0500

    Run for 10 times

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 CORE=19
 
-for N in {10000000,}
+for N in {10000000,1000000,100000,10000,1000,100}
 do
 	echo "#define N ${N}" > "n.h"
 	make clean && make
-	echo ${N}
-	for i in {1..2}
+	echo "N:" ${N}
+	for i in {1..10}
 	do
 		taskset -c ${CORE} ./benchmark.out
 	done
