443d695e672893dfdb9ec65b5eadc29245ae88b1
aarati-K <aarati.agc10@gmail.com>
2022-03-13 11:12:26 -0500

    Add run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CORE=1
+
+for N in {10000000,}
+do
+	echo "#define N ${N}" > "n.h"
+	make clean && make
+	echo ${N}
+	for i in {1..2}
+	do
+		taskset -c ${CORE} ./benchmark.out
+	done
+	echo "-------------"
+done
